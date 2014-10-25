d6cd2b7de727f8d7394208b3a3b6881cdc3cb360
Keng Kiat Lim <kkin@crnrc.com>
2014-10-26 01:39:06 +0800

    Altered main to receive arguments for seed and agent list

--- example_bots/raise_twenty_bot.py
+++ example_bots/raise_twenty_bot.py
@@ -1,5 +1,6 @@
 from bot import Bot
 
+
 class RaiseTwentyBot(Bot):
     def turn(self):
         return self.action('raise', amount=20)
--- main.py
+++ main.py
@@ -1,38 +1,54 @@
+import sys
 import time
 import platform
 import traceback
-import sys
 
+from argparse import ArgumentParser, REMAINDER as rem
+from importlib import import_module
 from poker_game import PokerGame
 
+
+def parse_args():
+    """
+    Parses the arguments from the command line
+    :return: An object with command-line arguments as attributes
+    """
+    parser = ArgumentParser()
+    parser.add_argument('--seed', help='A number to provide as a seed to the random number generator')
+    parser.add_argument('agents', help='A list of agents to simulate', nargs=rem)
+    return parser.parse_args()
+
+
+def file_name_to_class_name(file_name):
+    """
+    Converts underscored filenames to Camel-cased class names
+    :param file_name: String of the file name, not inclusive of the extension to be parsed
+    :return: A camel-cased string equivalent
+    """
+    components = file_name.title().split('_')
+    return ''.join(components)
+
+
 def main():
-   from example_bots.example_bot import FoldBot
-   from example_bots.raise_twenty_bot import RaiseTwentyBot
-   # from bots.callbot import CallBot
-   # from bots.jkbot import JKBot
-   # from example_bots.protobot.cpp.foldbot_cpp import FoldBotCpp
-   # from example_bots.protobot.java.foldbot_java import FoldBotJava
-   # from example_bots.protobot.java.boten_anna import Anna
-   # uncomment this line to run the CPP foldbot
-   #bots = [ExampleBot, FoldBotCpp]
-   seed = None
-   if len(sys.argv) > 1:
-       seed = int(sys.argv[1])
-   # bots = [FoldBot, RaiseTwentyBot, Anna]
-   bots = [FoldBot, RaiseTwentyBot]
-   game = PokerGame(bots=bots, seed=seed)
-   start_time = time.time()
-   outcome = game.run()
-   end_time = time.time()
-   print "Result:", outcome
-   print "Time elapsed: %0.2f seconds" % (end_time - start_time)
+    args = parse_args()
+    seed = int(args.seed) if args.seed is not None else None
+    agents = [getattr(import_module('agents.' + a), file_name_to_class_name(a)) for a in args.agents]
+
+    game = PokerGame(bots=agents, seed=seed)
+
+    start_time = time.time()
+    outcome = game.run()
+    end_time = time.time()
+
+    print "Result:", outcome
+    print "Time elapsed: %0.2f seconds" % (end_time - start_time)
 
 if __name__ == "__main__":
-    import sys
     try:
         sys.exit(main())
     except Exception, e:
         print ""
         traceback.print_exc()
+        
     if platform.system() == 'Windows':
         raw_input('\nPress enter to continue')
