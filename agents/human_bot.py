093e77a37d1d79326d04af1c9aa024f18aea2bf4
Davin Choo <cxjdavin@gmail.com>
2014-10-31 22:12:51 +0800

    Wrote helper_bot class to extract community and hand cards

--- agents/human_bot.py
+++ agents/human_bot.py
@@ -1,10 +1,11 @@
-from bot import Bot
+from helper_bot import HelperBot
 
-
-class HumanBot(Bot):
+class HumanBot(HelperBot):
     def turn(self):
+        print "Community Cards: " + str(self.get_community_cards())
+        print "Hand: " + str(self.get_hand())
+
         action = raw_input('Action: ').split()
-        print action
         if action[0] == 'raise':
             return self.action('raise', amount=int(action[1]))
         elif action[0] == 'check' or action[0] == 'call':
